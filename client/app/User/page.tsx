eace325a47d26d2b3e0b14aa86e4a5cb1896f430
HoriaMitrica <horia.mitrica@gmail.com>
2024-06-12 09:09:49 +0300

    project init

---
+++ client/app/User/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+    return (
+        <div className="bg-sky-800">
+            User
+        </div>
+    )
+}
+export default User
+
---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="m-8 font-bold bg-sky-400">
+    hello
+    </h1>
+    </>
+  );
+}
