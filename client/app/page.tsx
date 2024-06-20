8251b41ce51969439bbdf0eff8e029f6ec83f200
HoriaMitrica <horia.mitrica@gmail.com>
2024-06-20 07:40:19 +0300

    added redirect button

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,14 @@
-
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
     <h1 className="m-8 font-bold bg-sky-400">
-    hello
+    Homepage
     </h1>
+    <Link href="/Cube3x3">
+        <button>Go to Cube 3x3</button>
+      </Link>
     </>
   );
 }
